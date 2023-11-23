3249299bdbebd35849ab1a22db7d9c6056baf590
Erick <erickandrighetti08@hotmail.com>
2023-11-23 04:25:06 -0300

    so pra testar mesmo

---
+++ src/main/java/com/mycompany/teste/Appblak.java
@@ -0,0 +1,27 @@
+package com.mycompany.teste;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author 823135401
+ */
+import java.io.FileInputStream;
+import java.io.File;
+import java.util.Properties;
+public class Appblak {
+
+    public static void main(String[] args) throws Exception{
+        Properties properties = new Properties();
+        properties.load(new FileInputStream(new File("conf.properties")));
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new PrimeiraTela(properties).setVisible(true);
+            }
+        });
+    }
+}
+
